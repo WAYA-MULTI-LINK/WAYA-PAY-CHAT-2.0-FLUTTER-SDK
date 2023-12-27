f74ed67d13f20043e6cd7e8f120566ad1d7d912e
ayodejiEmmanuel <ayodeji.ayanbunmi@wayapaychat.com>
2023-12-28 00:18:28 +0100

    pub

--- lib/src/utils/extension.dart
+++ lib/src/utils/extension.dart
@@ -3,14 +3,24 @@ import 'package:currency_formatter/currency_formatter.dart';
 
 extension StringExtension on String {
   String toCurrency() {
-    CurrencyFormatter cf = CurrencyFormatter();
-    CurrencyFormatterSettings nairaSettings = CurrencyFormatterSettings(
+  //   // CurrencyFormatter cf = CurrencyFormatter();
+  //   const CurrencyFormat cf = CurrencyFormat(
+  //   // formatter settings for euro
+  //   code: 'eur',
+  //   symbol: '€',
+  //   symbolSide: SymbolSide.right,
+  //   thousandSeparator: '.',
+  //   decimalSeparator: ',',
+  //   symbolSeparator: ' ',
+  // );
+    // ignore: deprecated_member_use
+    CurrencyFormatterSettings nairaSettings = const CurrencyFormatterSettings(
       symbol:'NGN',
       symbolSide: SymbolSide.left,
       thousandSeparator: ',',
       decimalSeparator: '.',
     );
-    String formatted = cf.format(double.parse(this), nairaSettings);
+    String formatted =  CurrencyFormatter.format(double.parse(this), nairaSettings);
     return formatted;
   }
 
